d082df6bcc8a86f9832c0fda64c27a38f801a5df
nigelrudolf <mansell.nigel@mac.com>
2020-05-31 16:20:03 -0400

    basic layout complete

---
+++ script.js
@@ -0,0 +1,13 @@
+let audio = new Audio('assets/cute.mp3');
+audio.play();
+
+
+// Main screen plays audio in loop and awaits user to select play game or about
+// DigiPets
+
+// Play game
+// Happiness and Treat-0-Meter are set to 0
+// Petting increases happiness and treat-o-meter count
+// 10 pets = 1 treat
+// Clicking a trick when happiness is < required count results in a whimper to encourage more petting or treats
+// User should be informed somehow that there are no treats or tricks are not able to be performed
