340256aa0d884486932344372c0d680a7a3d181e
nigelrudolf <mansell.nigel@mac.com>
2020-05-31 17:50:35 -0400

    added hearts, hearts-#, treats, treats-# classes added functionality for increasing treats by petting

--- script.js
+++ script.js
@@ -1,12 +1,71 @@
-let audio = new Audio('assets/cute.mp3');
-audio.play();
+// let audio = new Audio('assets/cute.mp3');
+// audio.play();
+
+
+
+// let petCounter = 0;
+
+
+
+
+let petCounter = 0;
+
+$(document).ready(function(){
+    
+    let newGame = true;
+    
+    
+
+    if (newGame == true) {
+        $('.treats').hide();
+        $('.hearts').hide();
+    }
+
+    $('.pet-btn').click(function(){
+        petCounter++;
+
+        if (petCounter >= 5) {
+            $('.treat-1').show();
+        } 
+        if (petCounter >= 10) {
+            $('.treat-2').show();
+        } 
+        if (petCounter >= 15) {
+            $('.treat-3').show();
+        }
+
+        // $('.pet-btn').click(function(){
+        //     petCounter++;
+    
+        //     if (petCounter >= 5) {
+        //         $('.treat-1').show();
+        //     } 
+        //     if (petCounter >= 10) {
+        //         $('.treat-2').show();
+        //     } 
+        //     if (petCounter >= 15) {
+        //         $('.treat-3').show();
+        //     }
+    
+
+        // return petCounter;
+    });
+
+
+    
+
+    
+
+    
+    });
+
 
 
 // Main screen plays audio in loop and awaits user to select play game or about
 // DigiPets
 
 // Play game
-// Happiness and Treat-0-Meter are set to 0
+// Happiness and Treat-0-Meter are set to 1
 // Petting increases happiness and treat-o-meter count
 // 10 pets = 1 treat
 // Clicking a trick when happiness is < required count results in a whimper to encourage more petting or treats
